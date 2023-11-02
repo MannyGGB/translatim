48f78db1efd4017db30730a38c0b715d8d7bd7e5
Manuel Gonzalez Garcia de Blas <manugonzalez.tech@gmail.com>
2023-11-02 15:25:42 +0000

    Add translation functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //store out from and to languages in state
+  const [from, setFrom] = useState("en");
+  const [to, setTo] = useState("en");
+
+  //store the word we want to translate in state
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState("");
+
+  //on change function for the from and to states (we can write arrow functions directly into our select)
+  //on change function for the input of the word we want to translate (we can write arrow functions directly into our input)
+
+  //onSubmit function that calls our API to get the translation
+
+  async function handleTranslate(event) {
+    event.preventDefault();
+    const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
+    const res = await axios.get(API);
+    setTranslation(res.data.translation);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleTranslate}>
+        <div className="container">
+          <select onChange={(event) => setFrom(event.target.value)}>
+            <option value="en">English</option>
+            <option value="es">Spanish</option>
+            <option value="pl">Polish</option>
+            <option value="tr">Turkish</option>
+            <option value="ar">Arabic</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Translate"
+            onChange={(event) => setWord(event.target.value)}
+          />
+        </div>
+        <div className="container">
+          <select onChange={(event) => setTo(event.target.value)}>
+            <option value="en">English</option>
+            <option value="es">Spanish</option>
+            <option value="pl">Polish</option>
+            <option value="tr">Turkish</option>
+            <option value="ar">Arabic</option>
+          </select>
+          <div className="output">{translation}</div>
+        </div>
+        <button>Submit</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -8,4 +8,22 @@ const axios = require("axios");
 
 // add your endpoints here
 app.get("/", (request, response) => response.json("Root route for translatim"));
+
+app.get("/translate", async (request, response) => {
+  // const word = request.query.word;
+  // const from = request.query.from;
+  // const to = request.query.to;
+
+  const { word, from, to } = request.query; //destructure all the properties from request.query into variables
+
+  const API = `https://api.mymemory.translated.net/get?q=${word}!&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+  response.json(wrangledData);
+});
+
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
